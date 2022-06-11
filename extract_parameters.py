92dda82f5598cafd108d1c64ea244389dace3bd7
Krist Papadopoulos <kristpapas@gmail.com>
2022-06-11 15:03:29 -0400

    updated parameter extraction to python dictionary with option to save to pickle file

--- extract_parameters.py
+++ extract_parameters.py
@@ -1,19 +1,17 @@
 '''
-Extracting weights of Keras model layers from h5py file to csv
-
-Parameters from layers are extracted in rows with biases at end of parameters
-
-If layers have filters then parameters of filters are appended in the rows
+Extracting weights of Keras model layers from h5py file to dictionary with option
+to save to pickle file.
 
 Based on function for printing weights out:
 https://github.com/keras-team/keras/issues/91
 
 Args:
-    weight_path (str) : Path to the file to analyze
+    h5_path (str) : Path to the Keras model weights to analyze
       
-    csv_path (str):  CSV file with parameters and biases per layer  
+    output_path (str):  pkl output file path 
 
-Date: July 2, 2018
+Date: July 2, 2018 v0
+Date: June 11, 2022 v1
 
 Author: Krist Papadopoulos
 
@@ -21,28 +19,27 @@ Author: Krist Papadopoulos
 
 import argparse
 import h5py
-import pandas as pd
+import pickle
 
-parser = argparse.ArgumentParser(description='Extract Model Weights to CSV')
+parser = argparse.ArgumentParser(description='Extract model weights to dictionary')
 
 parser.add_argument('--h5_path', type=str, default=None, required=True, metavar='PATH',
                     help='path to parameter h5py file location (default: None)')
 
-parser.add_argument('--csv_path', type=str, default=None, required=True, metavar='PATH',
-                    help='path to csv output file location (default: None)')
+parser.add_argument('--output_path', type=str, default=None, required=True, metavar='PATH',
+                    help='path to parameter dictionary pkl output file location (default: None)')
 
 args = parser.parse_args()
-
     
 f = h5py.File(args.h5_path)
     
 try:
     if len(f.attrs.items()):
-        print("Parameter File: {}".format(args.h5_path))
-        print("Extracting Model Parameters to CSV File...")
+        print("Parameter file: {}".format(args.h5_path))
+        print("Extracting model parameters to parameter dictionary ...")
 
     if len(f.items())==0:
-        raise ValueError('No Layers with Paramters Found')
+        raise ValueError('No Layers with paramters found')
 
 except ValueError as ve:
     print(ve)
@@ -52,7 +49,6 @@ else:
     
     # each layer has a group
     for layer, group in f.items():
-            weights[layer] = []
         
         # in the group the keys are the members: either the layer bias and/or parameters
         for p_name in group.keys():
@@ -60,21 +56,18 @@ else:
             
             # if the group has 0 members then the layer has no biases or parameters
             if len(param) == 0:
-                weights[layer].extend(None)
+                weights[layer, None] = None
+            
             else:
-                
-                # for each parameter in the layer added values to a list: order is first biases then parameters
+                # for each parameter in the layer output to dictionary
                 for k_name in param.keys():
-                    weights[layer].extend(param[k_name].value[:].flatten().tolist())
+                    weights[layer, k_name] = param[k_name][()].tolist()
        
 finally:
     f.close()
-             
-csv_path = args.csv_path
-        
-weights_list = [[key] + value for key, value in weights.items()]
-                
-weights_df = pd.DataFrame(weights_list)
-        
-weights_df.to_csv(csv_path, index=False, header=False)
-        
+
+# save dictionary to pickle file
+if args.output_path:
+    with open(args.output_path, 'wb') as f:
+        pickle.dump(weights, f)
+              
